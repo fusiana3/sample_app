6ca8eae778b754a6b5b210d685bd401c6a7e4051
penta-P <145522425+penta-P@users.noreply.github.com>
2023-12-30 09:36:43 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    # => GET app/views/users/show.html.erb
   end
+
+  # GET /users/new or /signup
+  def new
+    @user = User.new
+    # => GET app/views/users/new.html.erb
+  end
+
+  def create
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    #Strong Parameters
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
